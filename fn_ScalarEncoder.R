4124036b9a2aadc7158d07ea0f3e5512ec377ec4
robeager <32424544+robeager@users.noreply.github.com>
2019-07-07 17:05:00 +1200

    added isDelta - boolean

--- fn_ScalarEncoder.R
+++ fn_ScalarEncoder.R
@@ -4,17 +4,21 @@
 #' @param n The number of total cells within the input matrix
 #' @param w The size (width) of the encoded bitmask
 #' @param resolution
+#' @param isDelta
 
 ScalarEncoder <- function(
   TotalBits = 400,
   BitMask = 21,
   EncodeResolution = 0.56,
-  EncodeBuckets = n-w+1
+  EncodeBuckets = n-w+1,
+  isDelta = FALSE
 ) {
   
   lowerBuffer = w
   upperBuffer = (n-w)
   minChange = 0
   maxChange = 232
+  
+  
   
 }
